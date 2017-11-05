d6ed30b8fb269faaf5dad10c48f78eb09752169b
Toz3wm <toz3wm@gmail.com>
2017-11-05 05:56:24 -0500

    structure du code

--- premierstests.py
+++ premierstests.py
@@ -1,1 +1,31 @@
-{\rtf1}
+import numpy as np
+import math
+import sys
+import cv2
+
+
+
+
+#2.1 Construction de la différence de gaussiennes
+def differenceDeGaussiennes(image_initiale, s, nb_octave):
+    pass
+
+
+#2.2 Détection des points clés
+def detectionPointsCles(DoG, sigma, seuil_contraste, r_courb_principale, resolution_octave):
+    pass
+
+
+
+#2.3 Descripteurs
+
+
+
+
+# pipeline Complet de l'algorithme
+def main():
+    #code goes here
+
+
+if __name__ == "__main__":
+    main()

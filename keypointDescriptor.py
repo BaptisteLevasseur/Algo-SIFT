73aecd0db9710d5818725a6d2c4cc832b8e8f079
BaptisteLevasseur <baptiste.levasseur@supelec.fr>
2017-12-04 22:00:32 -0500

    Orientation points cles

--- keypointDescriptor.py
+++ keypointDescriptor.py
@@ -14,14 +14,36 @@ from timeDecorator import timeit
 
 
 
-def orientationPointsCles(L_oct,points_cles,resolution_octave):
+def orientationPointCle(point_cle,L,sigma_list):
     # QUESTION : Quel sigma on choisi? La borne inférieure ou la borne supérieur? (car les points clés sont repérés
     # par rapport à la DoG et là on retourne sur L
-    L=L_oct[resolution_octave]
-    points_cles_liste = np.empty((0, 4))
-    for i in range(0, np.size(points_cles, 0)):
-        y, x, s = points_cles[i]
-        m = np.sqrt((L[y + 1, x, s] - L[y - 1, x, s]) ** 2 + (L[y, x + 1, s] - L[y, x - 1, s]) ** 2)
-        theta = np.arctan((L[y + 1, x, s] - L[y - 1, x, s]) / (L[y, x + 1, s] - L[y, x - 1, s]))
-        points_cles_liste = np.vstack((points_cles_liste, [y, x, m, theta]))
-    return points_cles_liste
+    y, x, s = point_cle # Pour le test
+    R = 4  # Rayon de la zone de voisinage
+    n = 2 * R + 1
+    L_grady, L_gradx = gradient(L[:, :, s])
+    G = gaussian_filter(n, 1.5 * sigma_list[s]).flatten()
+
+    points_cles_orientes = np.empty((0, 4))
+    # Calcul des matrices de d'amplitude et d'orientation pour chaque point du voisinage et conversion en vecteur
+    mat_m = np.sqrt((2 * L_gradx[y - R:y + R + 1, x - R:x + R + 1]) ** 2 + (
+    2 * L_grady[y - R:y + R + 1, x - R:x + R + 1]) ** 2).flatten()
+    mat_theta = np.arctan2(L_grady[y - R:y + R + 1, x - R:x + R + 1],
+                           L_gradx[y - R:y + R + 1, x - R:x + R + 1]).flatten() + np.pi
+
+    inervalles = np.linspace(0, 2 * np.pi, 37)  # 37 éléments donc 36 intervalles
+    hist = np.zeros(36)
+    for i in range(0, 36):
+        bool1 = mat_theta > inervalles[i]
+        bool2 = mat_theta < inervalles[i + 1]  # Obligé de créer des variables, triste langage de programmation..
+        # Numéros des pixels situés dans l'intervalle en question
+        pixels_inervalle = np.nonzero(bool1 & bool2)
+        # Pondération par la fenêtre gaussienne et normalisation par l'amplitude
+        hist[i] = sum(G[pixels_inervalle] / mat_m[pixels_inervalle])
+
+    orientations_ind = np.nonzero(hist > 0.8 * np.max(hist))
+    orientations = inervalles[orientations_ind] + (
+                                                  2 * np.pi / 36) / 2  # On prend la valeur médiane de l'intervalle (pas de 2pi/36)
+    for orientation in orientations:  # Peut être moyen de le faire plus court..
+        points_cles_orientes = np.vstack((points_cles_orientes, [y, x, s, orientation]))
+
+    return points_cles_orientes
--- test.py
+++ test.py
@@ -17,7 +17,7 @@ from scaleSpace import *
 image_initiale = mpimg.imread("lena.jpg")[:, :, 1]
 image_initiale = image_initiale / 255
 
-points_cles=np.array([[  2,  2 , 1],
+points_cles_list=np.array([[  2,  2 , 1],
  [  2,508 , 1],
  [ 72,291 , 1],
  [116,418 , 1],
@@ -136,44 +136,15 @@ s=3
 image = image_initiale[0::nb_octave+1, 0::nb_octave+1]
 L, sigma_list = pyramideDeGaussiennes(image, s, nb_octave)
 
+# points_cles_orientes_list = np.empty((0, 4))
+# for i in range(0,np.size(points_cles_list)):
+#  point_cle=points_cles_list[i] # Pour le test
+#  points_cles_orientes=orientationPointCle(point_cle,L,sigma_list)
+#  points_cles_orientes_list=np.concatenate(points_cles_orientes_list,points_cles_orientes)
+#
+# print(points_cles_orientes_list)
 
-
-
-
-y,x,s=points_cles[20] # Pour le test
-R=4 # Rayon de la zone de voisinage
-n=2*R+1
-L_grady,L_gradx=gradient(L[:,:,s])
-G=gaussian_filter(n,1.5*sigma_list[s]).flatten()
-
-
-keypoint_list= np.empty((0, 4))
-# Calcul des matrices de d'amplitude et d'orientation pour chaque point du voisinage et conversion en vecteur
-mat_m=np.sqrt((2*L_gradx[y-R:y+R+1,x-R:x+R+1])**2+(2*L_grady[y-R:y+R+1,x-R:x+R+1])**2).flatten()
-mat_theta=np.arctan2(L_grady[y-R:y+R+1,x-R:x+R+1],L_gradx[y-R:y+R+1,x-R:x+R+1]).flatten()+np.pi
-
-
-
-
-inervalles=np.linspace(0,2*np.pi,37) # 37 éléments donc 36 intervalles
-hist=np.zeros(36)
-for i in range(0,36):
- bool1=mat_theta > inervalles[i]
- bool2=mat_theta < inervalles[i+1] # Obligé de créer des variables, triste langage de programmation..
- # Numéros des pixels situés dans l'intervalle en question
- pixels_inervalle=np.nonzero(bool1 & bool2)
- # Pondération par la fenêtre gaussienne et normalisation par l'amplitude
- hist[i]=sum(G[pixels_inervalle]/mat_m[pixels_inervalle])
-
-orientations_ind=np.nonzero(hist > 0.8*np.max(hist))
-orientations=inervalles[orientations_ind]+(2*np.pi/36)/2 # On prend la valeur médiane de l'intervalle (pas de 2pi/36)
-for orientation in orientations: #Peut être moyen de le faire plus court..
- keypoint_list = np.vstack((keypoint_list, [y, x, s, orientation]))
-
-print(keypoint_list)
-
-# points_cles=orientationPointsCles([L],points_cles,nb_octave-1)
-
-
+print(orientationPointCle(points_cles_list[2],L,sigma_list))
+print(points_cles_list[0])
 
 
